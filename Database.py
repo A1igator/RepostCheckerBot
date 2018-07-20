718d523e971cc7acaa978521af1edceea1bf7ffe
AliAbdoli <xXAligatorXx@outlook.com>
2018-07-20 19:33:42 -0400

    added image checking and refined the bot overall.

--- Database.py
+++ Database.py
@@ -1,5 +1,11 @@
 import sqlite3
 import datetime
+from datetime import timedelta
+from calendar import monthrange
+from urllib.request import urlopen
+from io import BytesIO
+from PIL import Image
+import dhash
 
 def initDatabase(conn):
     c = conn.cursor()
@@ -7,38 +13,84 @@ def initDatabase(conn):
     conn.commit()
     c.close()
     print("Create table.")
-    
+
+def isInt(s):
+    try: 
+        int(s)
+        return True
+    except:
+        return False
+
+def monthdelta(d1, d2):
+    delta = 0
+    while True:
+        mdays = monthrange(d1.year, d1.month)[1]
+        d1 += timedelta(days=mdays)
+        if d1 <= d2:
+            delta += 1
+        else:
+            break
+    return delta
+
 def isLogged(conn, postImageUrl, postText, date):
     result = ""
     args = None
     c = conn.cursor()
     if postText != "":
-        print(postText)
-        args = c.execute("SELECT Url FROM Posts WHERE Content = ?;", (str(postText),))
-        result = list(args.fetchone())[0]
+        args = c.execute("SELECT COUNT(1) FROM Posts WHERE Content = ?;", (str(postText),))
+        if list(args.fetchone())[0] != 0:
+            args = c.execute("SELECT Url FROM Posts WHERE Content = ?;", (str(postText),))
+            result = list(args.fetchone())[0]
     elif postImageUrl != "":
-        args = c.execute("SELECT Url FROM Posts WHERE Content = ?;", (str(postImageUrl),))
-        result = list(args.fetchone())[0]
-    now = int(datetime.datetime.timestamp(datetime.datetime.today()))
-    then = int(date)
-    delta = now - then
-    print(delta)
-    if delta>15780000:
+        args = c.execute("SELECT COUNT(1) FROM Posts WHERE Content = ?;", (str(postImageUrl),))
+        if list(args.fetchone())[0] != 0:
+            args = c.execute("SELECT Url FROM Posts WHERE Content = ?;", (str(postImageUrl),))
+            result = list(args.fetchone())[0]
+        elif postImageUrl.endswith('png') or postImageUrl.endswith('jpg'):
+            file1 = BytesIO(urlopen(postImageUrl).read())
+            img1 = Image.open(file1)
+            args = c.execute("SELECT COUNT(1) FROM Posts WHERE Content = ?;", (str(dhash.dhash_int(img1)),))
+            if list(args.fetchone())[0] != 0:
+                args = c.execute("SELECT Url FROM Posts WHERE Content = ?;", (str(dhash.dhash_int(img1)),))
+                result = list(args.fetchone())[0]
+            else:
+                args = c.execute("SELECT Content, Url FROM posts;")
+                for hashed in args.fetchall():
+                    hashedReadable = hashed[0]
+                    if isInt(hashedReadable):
+                        hashedDifference = dhash.get_num_bits_different(dhash.dhash_int(img1), int(hashedReadable))
+                        if hashedDifference < 10:
+                            result = hashed[1]
+    now = datetime.datetime.today()
+    then = datetime.datetime.fromtimestamp(date)
+    timePassed = monthdelta(then, now)
+    print(timePassed)
+    if timePassed>6:
         if postUrl != "":
             c.execute("DELETE FROM Posts WHERE Content = ?;", (str(postImageUrl),))
         elif postText != "":
             c.execute("DELETE FROM Posts WHERE Content = ?;", (str(postText),))
         result = ""
+        print('deleted')
     c.close()
     print("Found? {}".format(result))
-    return result
+    if timePassed == 0:
+        return result, str((now-then).days) + ' days ago'
+    else:
+        return result, str(timePassed) + 'months ago'
 
-def addUser(conn, date, postImageUrl, postUrl, postText):
+def addUser(conn, date, postContentUrl, postUrl, postText):
     c = conn.cursor()
     if postText != "":
-        c.execute("INSERT INTO Posts (Date, Content, Url) VALUES (?, ?, ?);", (str(date), str(postText), str(postUrl),))
-    elif postImageUrl != "":
-        c.execute("INSERT INTO Posts (Date, Content, Url) VALUES (?, ?, ?);", (str(date), str(postImageUrl), str(postUrl),))
+        content = postText
+    else:
+        if postContentUrl.endswith('png') or postContentUrl.endswith('jpg'):
+            file1 = BytesIO(urlopen(postContentUrl).read())
+            img1 = Image.open(file1)
+            content = dhash.dhash_int(img1)
+        else:
+            content = postContentUrl
+        c.execute("INSERT INTO Posts (Date, Content, Url) VALUES (?, ?, ?);", (str(date), str(content), str(postUrl),))
     conn.commit()
     c.close()
     print("Added new post - {}".format(str(date)))
