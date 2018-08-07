bc13511c848fc7b70d9bc046d9199149bc637860
Ali Abdoli <xXAligatorXx@outlook.com>
2018-08-07 19:58:24 -0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,6 +3,6 @@ import os
 client_id     = os.environ['BOT_CLIENT_ID']
 client_secret = os.environ['BOT_CLIENT_SECRET']
 user_agent    = os.environ['BOT_USER_AGENT']
-username      = os.environ['BOT_USERNAME']
-password      = os.environ['BOT_PASSWORD']
+username      = os.environ['BOT_USER_USERNAME']
+password      = os.environ['BOT_USER_PASSWORD']
 subreddit     = os.environ['BOT_SUBREDDIT']
