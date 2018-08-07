9487ccdd326e29c4226c5554c96c5c9e7f722d2d
Ali Abdoli <xXAligatorXx@outlook.com>
2018-08-07 19:43:39 -0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,6 +1,6 @@
-client_id =
-client_secret =
-username =
-passowrd =
-user_agent =
-subreddit = 
+client_id     = os.environ['BOT_CLIENT_ID']
+client_secret = os.environ['BOT_CLIENT_SECRET']
+user_agent    = os.environ['BOT_USER_AGENT']
+username      = os.environ['BOT_USERNAME']
+password      = os.environ['BOT_PASSWORD']
+subreddit     = os.environ['BOT_SUBREDDIT']
