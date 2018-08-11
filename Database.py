afc245e7284d2ffb1bc8f549810fc1ab32f163fb
AliAbdoli <xXAligatorXx@outlook.com>
2018-08-11 12:00:53 -0400

    import change

--- Database.py
+++ Database.py
@@ -9,7 +9,7 @@ from io import BytesIO
 import ssl
 from PIL import Image
 import dhash
-import hashlib
+from hashlib import sha256
 
 reddit = praw.Reddit(client_id=Config.client_id,
                      client_secret=Config.client_secret,
