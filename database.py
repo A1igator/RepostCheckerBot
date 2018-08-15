157cae70d3e49c41a2303a85cc264233926e0a9b
AliAbdoli <xXAligatorXx@outlook.com>
2018-08-14 21:54:50 -0400

    testing

--- database.py
+++ database.py
@@ -106,6 +106,9 @@ def isLogged(conn, postContentUrl, postMedia, postText, date):
     now = datetime.datetime.utcnow()
     then = datetime.datetime.fromtimestamp(date)
     timePassed = (now-then).days
+    print(postContentUrl)
+    print(postMedia)
+    print(postText)
     if timePassed > config.days:
         delete(imgUrl)
         print('the post is older than needed')
