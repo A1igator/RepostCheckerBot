43c808268b78a668dfb4643889e957935b40f8a3
AliAbdoli <xXAligatorXx@outlook.com>
2018-08-17 00:39:16 -0400

    stopped script crashing when 503 from reddit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ import praw
 
 # packages that come with python
 import sqlite3
-import random 
+import random
+import sys
 
 # other files
 import config
@@ -23,52 +24,59 @@ c = conn.cursor()
 
 # the main function
 def findPosts():
-    print('Starting searching...')
-    post = 0
-    # first get 1000 posts from the top of the subreddit
-    for submission in subreddit.top('all', limit=1000):
-        post += 1
-        print('{} --> Starting new submission {}'.format(post, submission.id))
-        result = database.isLogged(conn, submission.url, submission.media, submission.selftext, submission.permalink, submission.created_utc)
-        if result != [['delete',-1,-1,-1]] and (result == [] or submission.created_utc != result[0][2]):
-            database.addPost(conn, submission.created_utc, submission.url, submission.media, submission.permalink, submission.selftext)
-            print('Added {}'.format(submission.permalink))
-    post = 0
-    # then get 10000 posts from new of the subreddit
-    for submission in subreddit.new(limit=1000):
-        post += 1
-        print('{} --> Starting new submission {}'.format(post, submission.id))
-        result = database.isLogged(conn, submission.url, submission.media, submission.selftext, submission.permalink, submission.created_utc)
-        if result != [['delete',-1,-1,-1]] and (result == [] or submission.created_utc != result[0][2]):
-            database.addPost(conn, submission.created_utc, submission.url, submission.media, submission.permalink, submission.selftext)
-            print('Added {}'.format(submission.permalink))
-    post = 0
-    # then check posts as they come in
-    for submission in subreddit.stream.submissions():
-        post += 1
-        print('{} --> Starting new submission {}'.format(post, submission.id))
-        result = database.isLogged(conn, submission.url, submission.media, submission.selftext, submission.permalink, submission.created_utc)
-        if result != [['delete',-1,-1,-1]] and (result == [] or submission.created_utc != result[0][2]):
-            database.addPost(conn, submission.created_utc, submission.url, submission.media, submission.permalink, submission.selftext)
-            print('Added {}'.format(submission.permalink))
-        if result != [] and result != [['delete',-1,-1,-1]] and post > 1:
-                print('reported')
-                # report and make a comment
-                submission.report('REPOST ALERT')
-                cntr = 0
-                table = ''
-                for i in result:
-                    table = table + str(cntr) + '|[post](https://reddit.com' + i[0] + ')|' + i[1] + '|' + str(i[3]) + '%' + '\n'
-                    cntr += 1
-                fullText = 'I have detected that this may be a repost: \n\nNum|Post|Date|Match\n:--:|:--:|:--:|:--:\n' + table + '\n*Beep Boop* I am a bot | [Source](https://github.com/xXAligatorXx/repostChecker) | Contact u/XXAligatorXx for inquiries.'
-                doThis = True
-                while doThis:
-                    try:
-                        submission.reply(fullText)
-                        doThis = False
-                    except:
+    while True:
+        try:
+            print('Starting searching...')
+            post = 0
+            # first get 1000 posts from the top of the subreddit
+            for submission in subreddit.top('all', limit=1000):
+                post += 1
+                print('{} --> Starting new submission {}'.format(post, submission.id))
+                result = database.isLogged(conn, submission.url, submission.media, submission.selftext, submission.permalink, submission.created_utc)
+                if result != [['delete',-1,-1,-1]] and (result == [] or submission.created_utc != result[0][2]):
+                    database.addPost(conn, submission.created_utc, submission.url, submission.media, submission.permalink, submission.selftext)
+                    print('Added {}'.format(submission.permalink))
+            post = 0
+            # then get 1000 posts from new of the subreddit
+            for submission in subreddit.new(limit=1000):
+                post += 1
+                print('{} --> Starting new submission {}'.format(post, submission.id))
+                result = database.isLogged(conn, submission.url, submission.media, submission.selftext, submission.permalink, submission.created_utc)
+                if result != [['delete',-1,-1,-1]] and (result == [] or submission.created_utc != result[0][2]):
+                    database.addPost(conn, submission.created_utc, submission.url, submission.media, submission.permalink, submission.selftext)
+                    print('Added {}'.format(submission.permalink))
+            post = 0
+            # then check posts as they come in
+            for submission in subreddit.stream.submissions():
+                post += 1
+                print('{} --> Starting new submission {}'.format(post, submission.id))
+                result = database.isLogged(conn, submission.url, submission.media, submission.selftext, submission.permalink, submission.created_utc)
+                if result != [['delete',-1,-1,-1]] and (result == [] or submission.created_utc != result[0][2]):
+                    database.addPost(conn, submission.created_utc, submission.url, submission.media, submission.permalink, submission.selftext)
+                    print('Added {}'.format(submission.permalink))
+                if result != [] and result != [['delete',-1,-1,-1]] and post > 1:
+                        print('reported')
+                        # report and make a comment
+                        submission.report('REPOST ALERT')
+                        cntr = 0
+                        table = ''
+                        for i in result:
+                            table = table + str(cntr) + '|[post](https://reddit.com' + i[0] + ')|' + i[1] + '|' + str(i[3]) + '%' + '\n'
+                            cntr += 1
+                        fullText = 'I have detected that this may be a repost: \n\nNum|Post|Date|Match\n:--:|:--:|:--:|:--:\n' + table + '\n*Beep Boop* I am a bot | [Source](https://github.com/xXAligatorXx/repostChecker) | Contact u/XXAligatorXx for inquiries.'
                         doThis = True
-
+                        while doThis:
+                            try:
+                                submission.reply(fullText)
+                                doThis = False
+                            except:
+                                doThis = True
+    except ValueError as e:
+        if '503' in str(e):
+            raise
+        else:
+            sys.exit(str(e))
+            break
 
 database.initDatabase(conn)
 findPosts()
