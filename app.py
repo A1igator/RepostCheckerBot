6d079f45c7fef46823b1196d3aa918012486ded7
AliAbdoli <xXAligatorXx@outlook.com>
2018-08-25 19:57:55 -0400

    test

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import sqlite3
 import random
 import sys
 import threading
-import time
 
 # other files
 import config
@@ -34,79 +33,65 @@ def deleteComment():
 
 def findPosts():
     conn = sqlite3.connect('Posts'+config.subreddit+'.db')
-    print('Starting searching...')
-    post = 0
-    # first get 1000 posts from the top of the subreddit
-    for submission in subreddit.top('all', limit=1000):
-        post += 1
-        print('{} --> Starting new submission {}'.format(post, submission.id))
-        result = database.isLogged(conn, submission.url, submission.media,
-                                   submission.selftext, submission.permalink, submission.created_utc)
-        if result != [['delete', -1, -1, -1]] and (result == [] or submission.created_utc != result[0][2]):
-            database.addPost(conn, submission.created_utc, submission.url,
-                             submission.media, submission.permalink, submission.selftext)
-            print('Added {}'.format(submission.permalink))
-    post = 0
-    # then get 1000 posts from new of the subreddit
-    for submission in subreddit.new(limit=1000):
-        post += 1
-        print('{} --> Starting new submission {}'.format(post, submission.id))
-        result = database.isLogged(conn, submission.url, submission.media,
-                                   submission.selftext, submission.permalink, submission.created_utc)
-        if result != [['delete', -1, -1, -1]] and (result == [] or submission.created_utc != result[0][2]):
-            database.addPost(conn, submission.created_utc, submission.url,
-                             submission.media, submission.permalink, submission.selftext)
-        print('Added {}'.format(submission.permalink))
-    post = 0
-    # then check posts as they come in
-    for submission in subreddit.stream.submissions():
-        post += 1
-        print('{} --> Starting new submission {}'.format(post, submission.id))
-        result = database.isLogged(conn, submission.url, submission.media,
-                                   submission.selftext, submission.permalink, submission.created_utc)
-        if result != [['delete', -1, -1, -1]] and (result == [] or submission.created_utc != result[0][2]):
-            database.addPost(conn, submission.created_utc, submission.url,
-                             submission.media, submission.permalink, submission.selftext)
-            print('Added {}'.format(submission.permalink))
-        if result != [] and result != [['delete', -1, -1, -1]] and post > 1:
-            print('reported')
-            # report and make a comment
-            submission.report('REPOST ALERT')
-            cntr = 0
-            table = ''
-            for i in result:
-                table = table + \
-                    str(cntr) + '|[post](https://reddit.com' + \
-                    i[0] + ')|' + i[1] + '|' + str(i[3]) + '%' + '\n'
-                cntr += 1
-            fullText = 'I have detected that this may be a repost: \n\nNum|Post|Date|Match\n:--:|:--:|:--:|:--:\n' + table + \
-                '\n*Beep Boop* I am a bot | [Source](https://github.com/xXAligatorXx/repostChecker) | Contact u/XXAligatorXx for inquiries | The bot will delete its message after -2 score'
-            doThis = True
-            while doThis:
-                try:
-                    submission.reply(fullText)
-                    doThis = False
-                except:
-                    doThis = True
-
-
-database.initDatabase(conn)
-deleteThread = threading.Thread(target=deleteComment)
-findThread = threading.Thread(target=findPosts)
-
-findThread.daemon = True
-
-deleteThread.start()
-
-while True:
-    if not findThread.is_alive():
+    while True:
         try:
-            findThread.start()
-            while True:
-                time.sleep(100)
+            print('Starting searching...')
+            post = 0
+            # first get 1000 posts from the top of the subreddit
+            for submission in subreddit.top('all', limit=1000):
+                post += 1
+                print('{} --> Starting new submission {}'.format(post, submission.id))
+                result = database.isLogged(conn, submission.url, submission.media,
+                                           submission.selftext, submission.permalink, submission.created_utc)
+                if result != [['delete', -1, -1, -1]] and (result == [] or submission.created_utc != result[0][2]):
+                    database.addPost(conn, submission.created_utc, submission.url,
+                                     submission.media, submission.permalink, submission.selftext)
+                    print('Added {}'.format(submission.permalink))
+            post = 0
+            # then get 1000 posts from new of the subreddit
+            for submission in subreddit.new(limit=1000):
+                post += 1
+                print('{} --> Starting new submission {}'.format(post, submission.id))
+                result = database.isLogged(conn, submission.url, submission.media,
+                                           submission.selftext, submission.permalink, submission.created_utc)
+                if result != [['delete', -1, -1, -1]] and (result == [] or submission.created_utc != result[0][2]):
+                    database.addPost(conn, submission.created_utc, submission.url,
+                                     submission.media, submission.permalink, submission.selftext)
+                    print('Added {}'.format(submission.permalink))
+            post = 0
+            # then check posts as they come in
+            for submission in subreddit.stream.submissions():
+                post += 1
+                print('{} --> Starting new submission {}'.format(post, submission.id))
+                result = database.isLogged(conn, submission.url, submission.media,
+                                           submission.selftext, submission.permalink, submission.created_utc)
+                if result != [['delete', -1, -1, -1]] and (result == [] or submission.created_utc != result[0][2]):
+                    database.addPost(conn, submission.created_utc, submission.url,
+                                     submission.media, submission.permalink, submission.selftext)
+                    print('Added {}'.format(submission.permalink))
+                if result != [] and result != [['delete', -1, -1, -1]] and post > 1:
+                    print('reported')
+                    # report and make a comment
+                    submission.report('REPOST ALERT')
+                    cntr = 0
+                    table = ''
+                    for i in result:
+                        table = table + \
+                            str(cntr) + '|[post](https://reddit.com' + \
+                            i[0] + ')|' + i[1] + '|' + str(i[3]) + '%' + '\n'
+                        cntr += 1
+                    fullText = 'I have detected that this may be a repost: \n\nNum|Post|Date|Match\n:--:|:--:|:--:|:--:\n' + table + \
+                        '\n*Beep Boop* I am a bot | [Source](https://github.com/xXAligatorXx/repostChecker) | Contact u/XXAligatorXx for inquiries | The bot will delete its message after -2 score'
+                    doThis = True
+                    while doThis:
+                        try:
+                            submission.reply(fullText)
+                            doThis = False
+                        except:
+                            doThis = True
 
-        # except KeyboardInterrupt as e:
-        #     print(e)
+        except KeyboardInterrupt:
+            sys.exit()
 
         except Exception as e:
             if '503' in str(e):
@@ -114,8 +99,14 @@ while True:
             else:
                 f = open('errs.txt', 'a')
                 f.write(str(e))
-            print('tesst')
-            sys.exit()
+
+
+database.initDatabase(conn)
+deleteThread = threading.Thread(target=deleteComment)
+findThread = threading.Thread(target=findPosts)
+
+deleteThread.start()
+findThread.start()
 
 deleteThread.join()
 findThread.join()
