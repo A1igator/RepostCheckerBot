3e5683fc030ad4611b271b7e500eda54f965e0bd
AliAbdoli <xXAligatorXx@outlook.com>
2018-08-28 17:55:52 -0400

    test

--- database.py
+++ database.py
@@ -307,6 +307,7 @@ def isLogged(conn, contentUrl, media, text, url, date):
 def addPost(conn, date, contentUrl, media, url, text):
     c = conn.cursor()
     if text != '&#x200B;':
+        print('test')
         content = hashText(text)
     else:
         if media != None:
