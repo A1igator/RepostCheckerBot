994ac4d7c0dc15c9062b1169ab36ad8e8cc7de1a
AliAbdoli <xXAligatorXx@outlook.com>
2018-09-15 13:47:38 -0400

    testing

--- config.py
+++ config.py
@@ -8,12 +8,13 @@ password = os.environ['BOT_PASSWORD']
 subSettings = [
     [
         os.environ['BOT_SUBREDDIT'],
-        int(6),
-        int(os.environ['BOT_HOT_DAYS']),
-        int(os.environ['BOT_NEW_DAYS']),
+        int(os.environ['BOT_TOP_DAYS']) if 'BOT_TOP_DAYS' in os.environ else None,
+        int(os.environ['BOT_HOT_DAYS']) if 'BOT_HOT_DAYS' in os.environ else None,
+        int(os.environ['BOT_NEW_DAYS']) if 'BOT_NEW_DAYS' in os.environ else None,
         int(os.environ['BOT_TOP_NUM_POSTS']) if 'BOT_TOP_NUM_POSTS' in os.environ else 1000,
         int(os.environ['BOT_HOT_NUM_POSTS']) if 'BOT_HOT_NUM_POSTS' in os.environ else 1000,
         int(os.environ['BOT_NEW_NUM_POSTS']) if 'BOT_NEW_NUM_POSTS' in os.environ else 1000,
         int(os.environ['BOT_THRESH'])
     ],
 ]
+print(subSettings)
