3659bf3f55ee7d1d5fddb4b0b9a7c98677874c61
AliAbdoli <xXAligatorXx@outlook.com>
2018-09-16 21:45:18 -0400

    testing

--- database.py
+++ database.py
@@ -143,7 +143,6 @@ def ignore():
 
 
 def addToFound(post, precentage):
-    print(post)
     result.append(post[0])
     originalPostDate.append(post[1])
     location.append(post[2])
@@ -216,8 +215,8 @@ def isLogged(conn, contentUrl, media, text, url, date, top, hot, new):
                     args = c.execute(
                         'SELECT Url, Date, Location FROM Posts WHERE Content = ?;', (str(text),))
                     fullResult = list(args.fetchall())
-                    print(fullResult)
                     for i in fullResult:
+                        print(i)
                         addToFound(i, 100)
                     args = c.execute(
                         'SELECT Url, Date, Location, Content FROM posts;')
