350a5afb9209f0fd51b5e15deb48cf3c8e688f6d
Ali Abdoli <xxaligatorxx@outlook.com>
2019-01-01 12:02:57 -0500

    adding one more sub

--- config.py
+++ config.py
@@ -38,5 +38,16 @@ subSettings = [
         int(os.environ['BOT_NEW_NUM_POSTS3']) if 'BOT_NEW_NUM_POSTS3' in os.environ else 1000,
         int(os.environ['BOT_THRESH3']) if 'BOT_THRESH3' in os.environ else 5,
         bool(os.environ['BOT_TEXT_IN_IMAGE3']) if 'BOT_TEXT_IN_IMAGE3' in os.environ else False,
-    ] if 'BOT_SUBREDDIT3' in os.environ else None,
+    ] if 'BOT_SUBREDDIT4' in os.environ else None,
+    [
+        os.environ['BOT_SUBREDDIT4'],
+        int(os.environ['BOT_TOP_DAYS4']) if 'BOT_TOP_DAYS4' in os.environ else None,
+        int(os.environ['BOT_HOT_DAYS4']) if 'BOT_HOT_DAYS4' in os.environ else None,
+        int(os.environ['BOT_NEW_DAYS4']) if 'BOT_NEW_DAYS4' in os.environ else None,
+        int(os.environ['BOT_TOP_NUM_POSTS4']) if 'BOT_TOP_NUM_POSTS4' in os.environ else 1000,
+        int(os.environ['BOT_HOT_NUM_POSTS4']) if 'BOT_HOT_NUM_POSTS4' in os.environ else 1000,
+        int(os.environ['BOT_NEW_NUM_POSTS4']) if 'BOT_NEW_NUM_POSTS4' in os.environ else 1000,
+        int(os.environ['BOT_THRESH4']) if 'BOT_THRESH4' in os.environ else 5,
+        bool(os.environ['BOT_TEXT_IN_IMAGE4']) if 'BOT_TEXT_IN_IMAGE4' in os.environ else False,
+    ] if 'BOT_SUBREDDIT4' in os.environ else None,
 ]
